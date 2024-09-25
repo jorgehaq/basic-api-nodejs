3ed5458117a24069ed798f1cd1ff155d8b726381
Jorge Alvarez <jorgehaq@gmail.com>
2024-09-24 21:19:40 -0700

    initial components

---
+++ index.js
@@ -0,0 +1,6 @@
+//import { suma } from './suma.mjs'
+const suma = require('./suma')
+
+console.log(suma(4,9));
+
+
---
+++ suma.js
@@ -0,0 +1,19 @@
+
+function suma(a,b){
+
+    return a + b
+
+}
+
+module.exports = suma
+/*
+
+module.exports ={
+    suma:function suma(a,b){
+
+        return a + b
+    
+    }
+
+}
+*/
